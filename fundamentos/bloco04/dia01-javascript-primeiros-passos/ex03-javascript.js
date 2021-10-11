1dd8516ec8f040b14e8c1d98ca59d9af0982ca6a
Renata <renata_castroaraujo@yahoo.com.br>
2021-10-11 18:19:56 -0300

    introduçao javascript

--- fundamentos/bloco04/dia01-javascript-primeiros-passos/ex03-javascript.js
+++ fundamentos/bloco04/dia01-javascript-primeiros-passos/ex03-javascript.js
@@ -2,7 +2,7 @@ const nota = 85;
  
 if (nota >= 80) {
     console.log("Parabens!Você foi aprovado")
-} else if (nota < 80 && nota > 60) {
+} else if (nota < 80 || nota > 60) {
     console.log ("Você está na lista de espera.")
 } else {
     console.log("Você foi reprovado.")

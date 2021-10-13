0ce6d157eb1710f0b7b4ca53fc9cb731961d94e7
Renata <renata_castroaraujo@yahoo.com.br>
2021-10-13 09:41:32 -0300

    mais exemplos javascript

---
+++ fundamentos/bloco04-Int-JavaScript-Logica/dia01-javascript-primeiros-passos/ex05-javascript.js
@@ -0,0 +1,10 @@
+let weekDay = "quarta-feira";
+
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe");
+} else if (weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe");
+} else {
+    console.log("FINALMENTE, descanso merecido ihuul")
+}
+ 

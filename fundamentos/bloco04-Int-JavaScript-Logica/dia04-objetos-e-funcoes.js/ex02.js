1ace477d15f4dde3fcd979261c2cd4453e96bb5d
Renata <renata_castroaraujo@yahoo.com.br>
2021-10-15 18:36:03 -0300

    exercicios objetos e funçoes

--- fundamentos/bloco04-Int-JavaScript-Logica/dia04-objetos-e-funcoes.js/ex02.js
+++ fundamentos/bloco04-Int-JavaScript-Logica/dia04-objetos-e-funcoes.js/ex02.js
@@ -1,8 +1,8 @@
 // Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
 let names = {
     person1: 'João',
-    person2:'Maria',
-    person3:'Jorge'
+    person2: 'Maria',
+    person3: 'Jorge'
 };
 
 for (let chave in names) {
@@ -11,12 +11,12 @@ for (let chave in names) {
 //----------------------------------------------------------------------------------------------------------//
 //Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
 let car = {
-    model:'A3 Sedan',
-    manufacturer:'Audi',
-    year:'2020'
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: '2020'
 };
 
-for (let chave in car){
+for (let chave in car) {
     console.log(chave, car[chave])
-}
+};
 

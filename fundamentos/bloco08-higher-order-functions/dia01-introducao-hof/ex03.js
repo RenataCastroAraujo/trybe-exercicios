e77ad8e83d82fbb20b6de98f6cd31db35fb86cd8
Renata <renata_castroaraujo@yahoo.com.br>
2021-11-19 14:59:47 -0300

    exemplos e testes bloco 08

---
+++ fundamentos/bloco08-higher-order-functions/dia01-introducao-hof/ex03.js
@@ -0,0 +1,19 @@
+//função como argumento para outra função
+
+const sayHello = () => {
+    return ('hello trybers');
+  }
+  
+  const printGreeting = (callback) => {
+      console.log(callback());
+  }
+  
+  printGreeting(sayHello);
+
+  //retornar uma função de outra função
+  const sumFixAmount = (amount) => {
+  return (number) => amount + number;
+}
+
+const initialSum = sumFixAmount(15)
+console.log(initialSum(5));
---
+++ fundamentos/bloco08-higher-order-functions/dia02-forEach-find-some-every-sort/ex02.js
@@ -0,0 +1,13 @@
+//exemplo com .sort
+
+
+//retorna em ordem decrescente
+const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => b - a);
+console.log(points); 
+
+
+//retorna em ordem crescente
+const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => a - b);
+console.log(points); 
